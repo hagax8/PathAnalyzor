5590676495ff9076a0cc1f684143c059316574c6
hagax8 <hagax8@gmail.com>
2017-11-14 16:38:06 +0000

    updated install packages

--- installPackages.sh
+++ installPackages.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 #packages for R manhattan/qqplots
-R -q -e "if (!'MASS' %in% installed.packages()) install.packages('MASS', repos='http://cran.rstudio.com/')"
-R -q -e "if (!'lattice' %in% installed.packages()) install.packages('lattice', repos='http://cran.rstudio.com/')"
-R -q -e "if (!'grid' %in% installed.packages()) install.packages('grid', repos='http://cran.rstudio.com/')"
-R -q -e "if (!'calibrate' %in% installed.packages()) install.packages('calibrate', repos='http://cran.rstudio.com/')"
-R -q -e "if (!'qqman' %in% installed.packages()) install.packages('qqman', repos='http://cran.rstudio.com/')"
-R -q -e "if (!'ggplot2' %in% installed.packages()) install.packages('ggplot2', repos='http://cran.rstudio.com/')"
-#packages for html tables/plots
-R -q -e "if (!'googleVis' %in% installed.packages()) install.packages('googleVis', repos='http://cran.rstudio.com/')"
+sudo R -q -e "if(! 'MASS' %in% installed.packages()){r = getOption('repos');r['CRAN'] = 'http://cran.uk.r-project.org'; options(repos = r); install.packages('MASS')}"
+sudo R -q -e "if(! 'lattice' %in% installed.packages()){r = getOption('repos');r['CRAN'] = 'http://cran.uk.r-project.org'; options(repos = r); install.packages('lattice')}"
+sudo R -q -e "if(! 'grid' %in% installed.packages()){r = getOption('repos');r['CRAN'] = 'http://cran.uk.r-project.org'; options(repos = r); install.packages('grid')}"
+sudo R -q -e "if(! 'calibrate' %in% installed.packages()){r = getOption('repos');r['CRAN'] = 'http://cran.uk.r-project.org'; options(repos = r); install.packages('calibrate')}"
+sudo R -q -e "if(! 'qqman' %in% installed.packages()){r = getOption('repos');r['CRAN'] = 'http://cran.uk.r-project.org'; options(repos = r); install.packages('qqman')}"
+sudo R -q -e "if(! 'ggplot2' %in% installed.packages()){r = getOption('repos');r['CRAN'] = 'http://cran.uk.r-project.org'; options(repos = r); install.packages('ggplot2')}"
+sudo R -q -e "if(! 'googleVis' %in% installed.packages()){r = getOption('repos');r['CRAN'] = 'http://cran.uk.r-project.org'; options(repos = r); install.packages('googleVis')}"
 
 #install perl packages 
 cpan Text::CSV::Simple
@@ -21,7 +20,7 @@ Pythondir=$(which python |sed 's/\//\\\//g' )
 for i in *.r; do sed -i.ba "1s/^.*$/\#\!${Rscriptdir}/" $i; done
 for i in *.py; do sed -i.ba "1s/^.*$/\#\!${Pythondir}/" $i; done 
 for i in *.pl; do sed -i.ba "1s/^.*$/\#\!${Perldir}/" $i; done
-
+for i in *.sh; do sed -i.ba "s/\/users\/k1507306\/localperl\/bin\/perl/perl/g" $i; done
 for i in *.r; do chmod u+x $i; done
 for i in *.py; do chmod u+x $i; done
 for i in *.pl; do chmod u+x $i; done
