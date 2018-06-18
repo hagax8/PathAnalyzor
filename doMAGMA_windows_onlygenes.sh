21899beb1995e09e495001bafbe881210b1ac81a
Héléna A. Gaspar <hagax8@gmail.com>
2018-06-18 18:47:12 +0100

    Update doMAGMA_windows_onlygenes.sh

--- doMAGMA_windows_onlygenes.sh
+++ doMAGMA_windows_onlygenes.sh
@@ -29,10 +29,6 @@ window1=$6
 window2=$7
 $magmaDir --annotate window=$window1,$window2 --snp-loc ${refgen} --gene-loc ${genloc} --out ${outname} 
 $magmaDir --bfile ${refgenbis} --pval ${pvalfile} N=$myN --gene-annot ${outname}.genes.annot --gene-model multi=snp-wise --out ${outname} --gene-settings snp-min-maf=0.01 
-#awk '{if (f==1) {r[$1]=$5"\t"$6"\t"$8"\t"$9} else if (($1 in r)){print r[$1]"\t"$0}}' f=1 ${outname}.genes.out f=2 $homeDir/biomart_noLRG > ${outname}.genes.out.names
-#grep "protein_coding" ${outname}.genes.out.names > ${outname}.genes.out.names.proteins
-#awk '{print  $6"\t"$7"\t"$11"\t"$4}' ${outname}.genes.out.names.proteins > ${outname}.prot.tmp
-#mv ${outname}.prot.tmp ${outname}.genes.out.names.proteins 
 
 awk '{if (f==1) {r[$1]=$5"\t"$6"\t"$8"\t"$9} else if (($1 in r)){print r[$1]"\t"$0}}' f=1 ${outname}.genes.out f=2 $homeDir/biomart_noLRG > ${outname}.genes.out.names
 
